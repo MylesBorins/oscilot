d3cdf148e152c36fbbd8f5760daca6ab25d8e47a
Myles Borins <myles.borins@gmail.com>
2020-04-25 01:49:55 -0400

    moar esm

--- server.mjs
+++ server.mjs
@@ -13,33 +13,35 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+import { on } from 'events';
 
-'use strict'
 import SocketIO from 'socket.io';
 import pino from 'pino';
 import { Client } from 'node-osc';
 
-import createStaticServer from './lib/static-server.cjs';
+import { createStaticServer } from './lib/static-server.mjs';
 
 const logger = pino({
   level: 'info'
 });
 
 const app = createStaticServer();
-
 const io = new SocketIO(app.server);
-
 const oscClient = new Client('127.0.0.1', 3333);
 
-io.on('connection', (socket) => {
-  logger.info('new session connected')
-  socket.on('message', (message) => {
-    logger.info(`address: ${message.address} value: ${message.value}`)
-    oscClient.send(message.address, Number(message.value));
-  });
-  socket.on('disconnect', () => {
-    logger.info('session disconnected');
-  });
-});
-
 app.listen(3000, (err) => console.error(err));
+
+async function main() {
+  for await (const [socket] of on(io, 'connection')) {
+    logger.info('new session connected');
+    socket.on('message', (message) => {
+      logger.info(`address: ${message.address} value: ${message.value}`)
+      oscClient.send(message.address, Number(message.value));
+    });
+    socket.on('disconnect', () => {
+      logger.info('session disconnected');
+    });
+  }
+}
+
+main().catch((e) => console.error(e));
